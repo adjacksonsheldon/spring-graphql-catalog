8ec5ffbb51d91ecb34caba22ea72950d95a31d75
Adjackson Sheldon da Paz Silva <adjacksonsheldon@gmail.com>
2026-02-28 11:41:57 -0300

    first commit

---
+++ src/test/java/com/asps/springgraphqlcatalog/spring_graphql_catalog/SpringGraphqlCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asps.springgraphqlcatalog.spring_graphql_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
